ba9d64d64e3b988b38e15b62b5204397595e9a54
abdulhay103 <abdulhay.me@gmail.com>
2024-06-02 10:47:40 +0600

    done

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className=" container mx-auto">
+      <div className=" w-full min-h-screen flex items-center justify-center">
+        <div className=" text-center">
+          <h2 className=" text-xl lg:text-2xl font-bold text-blue-800">
+            Wellcome to Unique Pain & Paralysis Centre.
+          </h2>
+          <div className=" py-3">
+            <p>This site is under constraction!</p>
+            <p>If any query please call- 01768377442</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
